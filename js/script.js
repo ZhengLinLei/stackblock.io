d4e9a39a4f780e471f6d9b4f26f7f892bb170675
david <sankeyr3@gmail.com>
2022-02-13 16:19:00 +0100

    Initial color scheme is now randomized

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let GAME_ = {
 // ==================================
 // DEFINE STACK COLOR
 const colorDesign = [
-    [30, 50, 70],
+    [Math.floor(Math.random()*360), 50, 70],
     [200, 80, 60]
 ];
 
@@ -194,6 +194,7 @@ window.addEventListener('load', ()=>{
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
     directionalLight.position.set(10, 20, 0);
+
     scene.add(directionalLight);
 
     // CAMERA
